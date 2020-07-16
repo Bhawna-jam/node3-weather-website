ae5a9f48b5a152b665a96d537ecdc45a4f4d0a50
Bhawna Khosla <bkhosla@linus.health>
2020-07-16 11:08:54 -0400

    Added to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longitude, callback) => {
         } else if (body.error){
             callback('unable to find location',undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.' + 'Humidity is ' + body.weather_descriptions.humidity)
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.' + 'Humidity is ' + body.current.humidity)
         }
     })
 }
